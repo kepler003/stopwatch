08407fa9956fbc26612935b84400a53e9a65e57a
Bartłomiej <b.pyzocha@gmail.com>
2021-12-16 23:26:22 +0100

    Bug: getTimeToMS give seconds & minutes > '60'

--- script.js
+++ script.js
@@ -2,20 +2,32 @@
 class Stopwatch {
   constructor(elem) {
     this.stopwatch = elem;
+    this.clock = this.stopwatch.querySelector('.stopwatch__time--current');
+    this.startTime = null;
+    this.curentTime = null;
+    this.interval = null;
+    // this.start();
   }
 
+  // start() {
+  //   this.startTime = Date.now();
+  //   setInterval(() => {
+  //     this.currentTime = Date.now();
+  //     this.renderTime(this.getTimeFromMS(this.currentTime - this.startTime));
+  //   }, 10);
+  // }
+
   renderTime({ minutes, seconds, centiseconds }) {
-    const para = this.stopwatch.querySelector('.stopwatch__time--current');
     if (centiseconds < 10) centiseconds = '0' + centiseconds;
     if (seconds < 10)      seconds = '0' + seconds;
     if (minutes < 10)      minutes = '0' + minutes;
-    para.textContent = `${minutes}:${seconds}:${centiseconds}`
+    this.clock.textContent = `${minutes}:${seconds}:${centiseconds}`
   }
   
   getTimeFromMS(ms) {
     const centiseconds = Math.floor(Math.floor(ms / 10) % 100);
-    const seconds = Math.floor(ms / 1000);
-    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor(Math.floor(ms / 1000) % 60);
+    const minutes = Math.floor(Math.floor(ms / 60000) % 60);
     return {
       centiseconds,
       seconds,
