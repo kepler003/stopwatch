a41566a32bf0c276ee9581cd142e04e2f7bb63bb
Bartłomiej <b.pyzocha@gmail.com>
2021-12-16 23:49:15 +0100

    Finish start & stop functionalities

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ class Stopwatch {
   constructor(elem) {
     this.stopwatch = elem;
     this.clock = this.stopwatch.querySelector('.stopwatch__time--current');
+    this.startBtn = this.stopwatch.querySelector('.stopwatch__btn--start');
+    this.pauseBtn = this.stopwatch.querySelector('.stopwatch__btn--pause');
+    this.markBtn = this.stopwatch.querySelector('.stopwatch__btn--mark');
+    this.resetBtn = this.stopwatch.querySelector('.stopwatch__btn--reset');
+
     this.startTime = null;
     this.curentTime = null;
     this.interval = null;
+
+    this.startBtn.addEventListener('click', () => this.start());
+    this.resetBtn.addEventListener('click', () => this.stop())
   }
 
   start() {
+    if (this.startTime) return;
     this.startTime = Date.now();
     this.interval = setInterval(() => {
       this.currentTime = Date.now();
@@ -17,15 +26,10 @@ class Stopwatch {
   }
 
   stop() {
+    this.startTime = null;
+    this.currentTime = null;
     clearInterval(this.interval);
     this.renderTime();
-  }
-
-  renderTime({ minutes = 0, seconds = 0, centiseconds = 0 } = {}) {
-    if (centiseconds < 10) centiseconds = '0' + centiseconds;
-    if (seconds < 10)      seconds = '0' + seconds;
-    if (minutes < 10)      minutes = '0' + minutes;
-    this.clock.textContent = `${minutes}:${seconds}:${centiseconds}`
   }
   
   getTimeFromMS(ms) {
@@ -38,7 +42,13 @@ class Stopwatch {
       minutes
     }
   }
+
+  renderTime({ minutes = 0, seconds = 0, centiseconds = 0 } = {}) {
+    if (centiseconds < 10) centiseconds = '0' + centiseconds;
+    if (seconds < 10)      seconds = '0' + seconds;
+    if (minutes < 10)      minutes = '0' + minutes;
+    this.clock.textContent = `${minutes}:${seconds}:${centiseconds}`
+  }
 }
 
-const stopwatchElem = document.querySelector('.stopwatch');
-new Stopwatch(stopwatchElem);
+new Stopwatch(document.querySelector('.stopwatch'));
